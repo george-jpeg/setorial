783d2cb11ba7fb1116c239ceb57cd6eecd511b90
wygeorgealt <otuturusolomom@gmail.com>
2026-09-18 21:10:20 +0100

    feat: iww33666

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -6,7 +6,12 @@ import { Pool } from 'pg';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
     constructor() {
-        const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+        const pool = new Pool({
+            connectionString: process.env.DATABASE_URL,
+            ssl: process.env.DATABASE_URL?.includes('sslmode=require')
+                ? { rejectUnauthorized: false }
+                : false,
+        });
         const adapter = new PrismaPg(pool);
         super({ adapter } as any);
     }
